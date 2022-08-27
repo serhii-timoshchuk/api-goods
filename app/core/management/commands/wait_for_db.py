98acd26e6ae9f24a14723592a4d8f70b9ec74d49
serhii timoshchuk <thegandalfgreat@gmail.com>
2022-08-27 23:42:26 +0300

    Fixed problems with default db start

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -17,10 +17,10 @@ class Command(BaseCommand):
         self.stdout.write('Waiting for database')
         db_up = False
         while db_up is False:
+            time.sleep(1)
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second ...')
-                time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
